1fb4dc0568de5588e8cf325c15ca73b8b67ca55a
Leo <10044837+tang-yu-xiao-bao@user.noreply.gitee.com>
2023-03-26 21:38:17 +0800

    Site updated: 2023-03-26 21:38:14

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://gitee.com/tang-yu-xiao-bao/image-bad/raw/master/Blog/dm14.webp);
+    --darkmode-bg:url(https://gitee.com/tang-yu-xiao-bao/image-bad/raw/master/Blog/yuanshen1.webp);
+    --mobileday-bg: url(https://gitee.com/tang-yu-xiao-bao/image-bad/raw/master/Blog/snow.webp);
+    --mobilenight-bg: url(https://gitee.com/tang-yu-xiao-bao/image-bad/raw/master/Blog/mb8.webp);
   }`;
 }
 // 切换背景主函数
